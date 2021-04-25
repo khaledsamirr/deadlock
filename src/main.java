3b0c8b0ee379d918b51c1b5ffde36a69b85f84d4
Youssef Ahmed <youssef9300@hotmail.com>
2021-04-25 14:01:38 +0200

    Re Initialize

--- src/main.java
+++ src/main.java
@@ -1,72 +1,96 @@
+import java.util.ArrayList;
+
 public class main {
-    int numOfProcesses = 5;
-    int numOfResources = 3;
-    int need[][] = new int[numOfProcesses][numOfResources];
-    int [][]allocated= {{1,1,0},{2,0,0},{2,0,1},{2,0,2},{0,1,2}};
-    int [][]max={{5,5,3},{3,2,3},{8,0,2},{4,2,2},{5,3,5}};
-    int []available={2,5,4};
-    int safe[] = new int[numOfProcesses];
-    boolean isSafe() {
-        int count=0;
-        boolean b[] = new boolean[numOfProcesses];
-        for (int i = 0;i < numOfProcesses; i++)
-        {
-            b[i] = false;
+    public static void main(String args[]) {
+        int numOfProcesses = 5;
+        int numOfResources = 3;
+        int need[][] = new int[numOfProcesses][numOfResources];
+        int allocated[][] = {{1, 1, 0}, {2, 0, 0}, {2, 0, 1}, {2, 0, 2}, {0, 1, 2}};
+        int max[][] = {{5, 5, 3}, {3, 2, 3}, {8, 0, 2}, {4, 2, 2}, {5, 3, 5}};
+        int available[] = {2, 5, 4};
+        Boolean deadLockState = true;
+        String processesFinishSequence = "";
+        String processesDeadLockSequence = "";
+        Boolean finish[] = new Boolean[numOfProcesses];
+        int allFinishCounter = 0;
+
+        for (int i = 0; i < numOfProcesses; i++) {
+            finish[i] = false;
         }
 
-        int work[] = new int[numOfResources];
-        for (int i = 0;i < numOfResources; i++)
-        {
-            work[i] = available[i];
-        }
+        need = calcNeed(allocated, max, need);
 
-        while (count<numOfProcesses)
-        {
-            boolean flag = false;
-            for (int i = 0;i < numOfProcesses; i++)
-            {
-                if (b[i] == false)
-                {
-                    int k;
-                    for (k = 0;k < numOfResources; k++)
-                    {
-                        if (need[i][k] > work[k])
+        while (true) {
+            int j = 0;
+
+            for (int i = 0; i < need.length; i++) {
+
+                if (!finish[i]) {
+
+                    processesDeadLockSequence += i;
+
+                    for (j = 0; j < need[0].length; j++) {
+                        if (need[i][j] > available[j]) {
                             break;
-                    }
-                    if (k == numOfResources)
-                    {
-                        safe[count++]=i;
-                        b[i]=true;
-                        flag=true;
-
-                        for (k = 0;k < numOfResources; k++)
-                        {
-                            work[k] = work[k]+allocated[i][k];
                         }
                     }
+
+                    if (j >= numOfResources) {
+                        for (int q = 0; q < numOfResources; q++) {
+                            available[q] += allocated[i][q] + need[i][q];
+                            allocated[i][q] = 0;
+                            need[i][q] = 0;
+                        }
+                        processesFinishSequence += i;
+                        finish[i] = true;
+                        processesDeadLockSequence = "";
+                    }
+                }
+
+            }
+
+            for (int i = 0; i < finish.length; i++) {
+                if (finish[i]) {
+                    allFinishCounter++;
                 }
             }
-            if (flag == false)
-            {
+
+            if (allFinishCounter >= finish.length) {
+                deadLockState = false;
                 break;
             }
+
+            if (processesDeadLockSequence.length() + allFinishCounter >= need.length) {
+                deadLockState = true;
+                break;
+            }
+
         }
-        if (count < numOfProcesses)
-        {
-            return false;
-        }
-        else
-        {
-            return true;
-        }
-    }
-    void calcNeed() {
-        for (int i = 0;i < numOfProcesses; i++)
-        {
-            for (int j = 0;j < numOfResources; j++)
-            {
-                need[i][j] = max[i][j]-allocated[i][j];
+
+
+        if (deadLockState) {
+            System.out.println("DeadLock Sequence:");
+            for (int i = 0; i < numOfProcesses; i++) {
+                System.out.print(" P" + processesDeadLockSequence.charAt(i));
+            }
+        } else if (!deadLockState) {
+            System.out.println("Finished Sequence:");
+            for (int i = 0; i < numOfProcesses; i++) {
+                System.out.print(" P" + processesFinishSequence.charAt(i));
             }
         }
+
     }
+
+
+    public static int[][] calcNeed(int allocated[][], int max[][], int need[][]) {
+        for (int i = 0; i < allocated.length; i++) {
+            for (int j = 0; j < allocated[i].length; j++) {
+                need[i][j] = max[i][j] - allocated[i][j];
+            }
+        }
+
+        return need;
+    }
+
 }
